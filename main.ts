a528f4afc78eb6f2d90e44a91bdd620f40ad1367
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-05 21:52:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+game.splash("Let's calculate the area of a trapezoid")
+let Top_length = game.askForNumber("Enter the top length of trapezoid")
+let Bottom_length = game.askForNumber("Enter the Bottom length of trapezoid")
+let Height = game.askForNumber("Enter the Height of trapezoid")
+let Area = (Top_length + Bottom_length) * (Height / 2)
+game.splash("Area of trapezoid dimensions: " + " (" + Top_length + "cm + " + Bottom_length + "cm /2) x " + Height + "cm " + convertToText(Area))
